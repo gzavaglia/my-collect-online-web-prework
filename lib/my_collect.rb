115e5d316df621d2967d61a00a3b218dcb415d41
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 18:33:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  i = 0 
+  while i < array.length 
+    new_array = []
+    new_array = yield array[i]
+    i = i+1
 
+  end #end while 
+  
+  
+  
+end #end my_collect()
 
